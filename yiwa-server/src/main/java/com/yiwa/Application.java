3961747aefeb28051f22c0af878e55e6b800a7e2
Caesar <ck9300001@qq.com>
2021-05-23 16:54:50 +0800

    增加启动Banner

--- yiwa-server/src/main/java/com/yiwa/Application.java
+++ yiwa-server/src/main/java/com/yiwa/Application.java
@@ -5,7 +5,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Environment;
 
 /**
  * 启动类
@@ -19,15 +18,6 @@ public class Application {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Application.class);
-        Environment env = context.getEnvironment();
-        log.info("====================================================================");
-        log.info("项目版本: {}", env.getProperty("project.version"));
-        log.info("启动环境: {}", env.getProperty("project.env"));
-        log.info("启动端口: {}", env.getProperty("server.port"));
-        log.info("日志等级: {}", env.getProperty("logback.level"));
-        log.info("日志Appender: {}", env.getProperty("logback.appender"));
-        log.info("Swagger: {}", Boolean.valueOf(env.getProperty("swagger.enabled")) ? "启用" : "禁用");
-        log.info("Startup complete ...");
-        log.info("====================================================================");
+        context.getEnvironment();
     }
 }
