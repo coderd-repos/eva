c304d24bf42796f41410c777597cf63817d743f1
Caesar <ck9300001@qq.com>
2021-05-24 16:13:48 +0800

    调整是否包含指定权限方法、增加是否包含角色方法

--- eva-front/src/components/base/BasePage.vue
+++ eva-front/src/components/base/BasePage.vue
@@ -6,6 +6,21 @@ export default {
     ...mapState(['userInfo'])
   },
   methods: {
+    // 是否包含指定角色
+    containRoles (roles) {
+      if (this.userInfo == null) {
+        return false
+      }
+      if (this.userInfo.roles == null || this.userInfo.roles.length === 0) {
+        return false
+      }
+      for (const code of roles) {
+        if (this.userInfo.roles.findIndex(r => r === code) > -1) {
+          return true
+        }
+      }
+      return false
+    },
     // 是否包含指定权限
     containPermissions (permissions) {
       if (this.userInfo == null) {
@@ -15,7 +30,7 @@ export default {
         return false
       }
       for (const code of permissions) {
-        if (this.userInfo.permissions.findIndex(p => p.code === code) > -1) {
+        if (this.userInfo.permissions.findIndex(p => p === code) > -1) {
           return true
         }
       }
