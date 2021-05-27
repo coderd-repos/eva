05dd0d838dce18b55839b3c0a6e21a2c7a11e93e
Caesar <ck9300001@qq.com>
2021-05-27 22:34:49 +0800

    全局异常处理方式调整

--- eva-server/src/main/java/com/eva/api/BaseController.java
+++ eva-server/src/main/java/com/eva/api/BaseController.java
@@ -1,24 +1,8 @@
 package com.eva.api;
 
-import com.eva.core.model.ApiResponse;
-import com.eva.core.exception.BusinessException;
 import com.eva.core.model.LoginUserInfo;
-import com.alibaba.fastjson.JSON;
-import com.eva.core.model.ResponseStatus;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authz.UnauthorizedException;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Controller基类
@@ -36,44 +20,5 @@ public class BaseController {
     protected LoginUserInfo getLoginUser () {
         return (LoginUserInfo)SecurityUtils.getSubject().getPrincipal();
     }
-  
-    /**
-     * 全局异常捕获
-     */
-    @ExceptionHandler(Exception.class)
-    public ModelAndView exceptionHandle(HttpServletResponse response, Exception ex){
-        try {
-            log.error(ex.getMessage(), ex);
-            response.setHeader("content-type", "application/json;charset=UTF-8");
-            // 业务异常
-            if (ex instanceof BusinessException) {
-                BusinessException be = (BusinessException) ex;
-                response.getWriter().write(JSON.toJSONString(ApiResponse.failed(be.getCode(), be.getMessage())));
-                return null;
-            }
-            // 无权限异常
-            if (ex instanceof UnauthorizedException) {
-                response.getWriter().write(JSON.toJSONString(ApiResponse.failed("没有操作权限")));
-                return null;
-            }
-            // 验证异常
-            if (ex instanceof MethodArgumentNotValidException) {
-                MethodArgumentNotValidException exs = (MethodArgumentNotValidException)ex;
-                BindingResult bindingResult = exs.getBindingResult();
-                List<String> errors = new ArrayList<>();
-                for(FieldError fieldError : bindingResult.getFieldErrors()){
-                    errors.add(fieldError.getDefaultMessage());
-                }
-                response.getWriter().write(JSON.toJSONString(ApiResponse.failed(ResponseStatus.BAD_REQUEST.getCode(), StringUtils.join(errors))));
-                return null;
-            }
-            response.getWriter().write(JSON.toJSONString(ApiResponse.failed(ex.getMessage() == null ? "系统繁忙" : ex.getMessage())));
-            return null;
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-            ModelMap model = new ModelMap();
-            model.addAttribute("error", "未知异常");
-            return new ModelAndView("error/default", model);
-        }
-    }
+
 }
---
+++ eva-server/src/main/java/com/eva/config/handler/GlobalExceptionHandler.java
@@ -0,0 +1,59 @@
+package com.eva.config.handler;
+
+import com.eva.core.exception.BusinessException;
+import com.eva.core.model.ApiResponse;
+import com.eva.core.model.ResponseStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.authz.UnauthorizedException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 全局异常处理
+ * 技术参考：https://blog.csdn.net/typ1805/article/details/107283602
+ * @author Eva
+ * @date 2021-05-27 22:21
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 业务异常处理
+     */
+    @ExceptionHandler(BusinessException.class)
+    public <T> ApiResponse<T> handleBusinessException (BusinessException e) {
+        log.error(e.getMessage(), e);
+        return ApiResponse.failed(e.getCode(), e.getMessage());
+    }
+
+    /**
+     * 无权限异常处理
+     */
+    @ExceptionHandler(UnauthorizedException.class)
+    public <T> ApiResponse<T> handleUnauthorizedException (UnauthorizedException e) {
+        log.error(e.getMessage(), e);
+        return ApiResponse.failed("没有操作权限");
+    }
+
+    /**
+     * 参数验证未通过异常处理
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public <T> ApiResponse<T> handleMethodArgumentNotValidException (MethodArgumentNotValidException e) {
+        log.error(e.getMessage(), e);
+        BindingResult bindingResult = e.getBindingResult();
+        List<String> errors = new ArrayList<>();
+        for(FieldError fieldError : bindingResult.getFieldErrors()){
+            errors.add(fieldError.getDefaultMessage());
+        }
+        return ApiResponse.failed(ResponseStatus.BAD_REQUEST.getCode(), StringUtils.join(errors));
+    }
+}
