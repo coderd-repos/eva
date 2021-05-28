ea0823747c0825663acaa5e3a2731744a9198ddb
Caesar <ck9300001@qq.com>
2021-05-28 15:44:42 +0800

    登录图片不展示问题修复

--- eva-front/src/views/login.vue
+++ eva-front/src/views/login.vue
@@ -110,7 +110,7 @@ $input-gap: 30px;
   display: flex;
   width: 100%;
   height: 100vh;
-  background-image: url("/assets/images/login.jpg");
+  background-image: url("../assets/images/login.jpg");
   background-repeat: no-repeat;
   background-size: auto 180%;
   background-clip: content-box;
