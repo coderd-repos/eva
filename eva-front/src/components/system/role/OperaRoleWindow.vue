4a72af1cd4d343cbd980bbadca81b69a07897922
Caesar <ck9300001@qq.com>
2021-05-30 21:11:09 +0800

    权限修改编码时给出二次确认、角色修改编码时给出二次确认

--- eva-front/src/components/system/role/OperaRoleWindow.vue
+++ eva-front/src/components/system/role/OperaRoleWindow.vue
@@ -28,6 +28,8 @@ export default {
   components: { GlobalWindow },
   data () {
     return {
+      // 原角色码
+      originRoleCode: '',
       // 表单数据
       form: {
         id: null,
@@ -44,6 +46,45 @@ export default {
           { required: true, message: '请输入角色名称' }
         ]
       }
+    }
+  },
+  methods: {
+    open (title, target) {
+      this.title = title
+      this.visible = true
+      // 新建
+      if (target == null) {
+        this.$nextTick(() => {
+          this.$refs.form.resetFields()
+          this.form[this.configData['field.id']] = null
+        })
+        return
+      }
+      // 编辑
+      this.$nextTick(() => {
+        this.originRoleCode = target.code
+        for (const key in this.form) {
+          this.form[key] = target[key]
+        }
+      })
+    },
+    confirm () {
+      if (this.form.id == null || this.form.id === '') {
+        this.__confirmCreate()
+        return
+      }
+      if (this.originRoleCode === this.form.code) {
+        this.__confirmEdit()
+        return
+      }
+      // 修改了角色编码
+      this.$dialog.confirm('检测到您修改了角色编码，角色编码修改后前后端均可能需要调整代码，确认修改吗？', '提示', {
+        confirmButtonText: '确认修改',
+        type: 'warning'
+      })
+        .then(() => {
+          this.__confirmEdit()
+        })
     }
   },
   created () {
